2a8364d3aee585aed60a13fb22db9225258b2c1e
beatricia <beatricia.inspirit@gmail.com>
2020-09-18 18:01:49 +0800

    Transferred css to css folder

---
+++ SIM-OpenHouse/src/pages/ForgetPassword.tsx
@@ -0,0 +1,26 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import ExploreContainer from '../components/ExploreContainer';
+import '../css/ForgetPassword.css';
+
+const ForgetPassword: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>ForgetPassword</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Blank</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <ExploreContainer />
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default ForgetPassword;
