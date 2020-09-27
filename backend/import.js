4e98f1fa9323fab1153c199a2ec367b416795c70
Vincent Yap <the-mazer@hotmail.com>
2020-09-27 19:55:43 +0800

    updated import.js

--- backend/import.js
+++ backend/import.js
@@ -2,35 +2,39 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./fypServiceKey.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://fyp20s3-13.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fyp20s3-13.firebaseio.com",
 });
 
-const db  = admin.firestore();
+const db = admin.firestore();
 
 const fs = require("fs");
 const path = require("path");
-const pathDir = './data/';
+const pathDir = "./data/";
 
 const args = process.argv.slice(2);
 
 fs.readdir(pathDir, (err, files) => {
-    if (err)
-        return console.log(`${err} Error reading directory`)
+  if (err) return console.log(`${err} Error reading directory`);
+  let read = false;
+  files.forEach((file) => {
+    var fileName = path.parse(file).name;
 
-    files.forEach(file => {
-        var fileName = path.parse(file).name;
-
-        if (fileName == args) {
-            let dataCol = require(`${pathDir}${file}`)
-
-            dataCol.forEach(obj => {
-                db.collection(fileName).doc(obj.id).set(obj).then(ref => {
-                    console.log(`${ref} added`)
-                }).catch(err => {
-                    console.log("Error occured:", err);
-                });
-            });
-        }
-    });
+    if (fileName == args) {
+      let dataCol = require(`${pathDir}${file}`);
+      read = true;
+      dataCol.forEach((obj) => {
+        db.collection(fileName)
+          .doc(obj.id)
+          .set(obj)
+          .then((ref) => {
+            console.log(`${ref} added`);
+          })
+          .catch((err) => {
+            console.log("Error occured:", err);
+          });
+      });
+    }
+  });
+  if (read == false) console.log(`File ${args} does not exist`);
 });
