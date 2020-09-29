4b26298d5e2f997f3544f223859d678cea07b641
Vincent Yap <the-mazer@hotmail.com>
2020-09-29 21:43:07 +0800

    programmeTalk link to DB

--- backend/import.js
+++ backend/import.js
@@ -7,28 +7,27 @@ const pathDir = "./data/";
 const args = process.argv.slice(2)[0];
 
 fs.readdir(pathDir, (err, files) => {
-    if (err) return console.log(`${err} Error reading directory`);
-    let read = false;
+  if (err) return console.log(`${err} Error reading directory`);
+  let read = false;
 
-    files.forEach(file => {
-        let fileName = path.parse(file).name;
-        let fileExt = path.parse(file).ext;
+  files.forEach(file => {
+    let fileName = path.parse(file).name;
+    let fileExt = path.parse(file).ext;
 
-        if (fileName == args && fileExt == '.json') {
-            let dataCol = require(`${pathDir}${file}`);
-            let counter = 0;
+    if (fileName == args && fileExt == '.json') {
+      let dataCol = require(`${pathDir}${file}`);
+      let counter = 0;
 
-        dataCol.forEach((obj) => {
-          db.collection(fileName)
-            .doc(obj.id)
-            .set(obj)
-            .catch((err) => {
-              console.log(`Error occured: ${err}`);
-            });
-          counter++;
-        });
-        console.log(`${counter} documents added to ${fileName}`);
-      }
-    });
+      dataCol.forEach((obj) => {
+        db.collection(fileName)
+          .doc(obj.id)
+          .set(obj)
+          .catch((err) => {
+            console.log(`Error occured: ${err}`);
+          });
+        counter++;
+      });
+      console.log(`${counter} documents added to ${fileName}`);
+    }
   });
 });
