60ae2900dd0c7c646042a7a3ff6e3cec31ff86f8
Vincent Yap <the-mazer@hotmail.com>
2020-10-24 03:38:30 +0800

    added filter details and fixed compare

--- SIM-OpenHouse/src/pages/Study@SIM/Study@SIM_Routing.tsx
+++ SIM-OpenHouse/src/pages/Study@SIM/Study@SIM_Routing.tsx
@@ -2,13 +2,16 @@ import { IonRouterOutlet } from '@ionic/react';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, RouteComponentProps } from 'react-router-dom';
 
+
 import '../../css/Global.css';
 import StudySIMMain from './Study@SIMMain';
 import StudySIMProgList from './Study@SIMProgList';
 import StudySIMProgInfo from './Study@SIMProgInfo';
 import CourseComparator from './CourseComparator';
 
+
 const StudySIM_Routing: React.FC<RouteComponentProps> = ({ match }) => {
+
 
     return (
         <IonRouterOutlet>
@@ -16,6 +19,7 @@ const StudySIM_Routing: React.FC<RouteComponentProps> = ({ match }) => {
                 <Route exact path='/u/study@SIMMain' component={StudySIMMain} />
                 <Route path={`/u/study@SIMMain/Study@SIMProgInfo/:id/:discipline/:category`} component={StudySIMProgInfo} />
                 <Route path={`/u/study@SIMMain/:discipline/:category/courseComparator`} component={CourseComparator} />
+                {/* <Route path={`/u/study@SIMMain/:discipline/:category`} render={props => <StudySIMProgList {...props} compareProgList={compareProgList} onCompareProgListChange={onCompareProgListChange} />} /> */}
                 <Route path={`/u/study@SIMMain/:discipline/:category`} component={StudySIMProgList} />
             </Switch>
         </IonRouterOutlet>
