a58e2a25334902a6c2eeef9bbd2f258f0caab787
beatricia <beatricia.inspirit@gmail.com>
2020-11-14 04:32:13 +0800

    Updated iframe for video

--- sim-openhouse/src/components/OpenHouseProgrammes/ProgTalkLive.tsx
+++ sim-openhouse/src/components/OpenHouseProgrammes/ProgTalkLive.tsx
@@ -21,14 +21,17 @@ const ProgTalkLive: React.FC = () => {
                 <TopNav title="Live Talk" route="/u/openHouseMain/programmeTalks" backarrow={true} hamburger={true} />
             </IonHeader>
 
-            <IonContent fullscreen className="progTalkIonContent">              
-                <IonGrid className="iframeGrid">
-                    <IonRow style={{paddingTop: "2%"}}>
+            <IonContent fullscreen className="progTalkIonContent">            
+                <IonGrid className="progTalkIframeGrid">
+                    <IonRow className="ion-justify-content-center">
                         <IonCol size="12" sizeSm="12" className="ion-text-center">
-                            <iframe src={`https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F${fbid}%2Fvideos%2F${id}%2F&width=360`} className="iframVid" scrolling="no" frameBorder="0" allowFullScreen></iframe>
+                            <div style={{height: "50%"}}>
+                                <iframe src={`https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F${fbid}%2Fvideos%2F${id}%2F&width=360`} className="iframeVideo" scrolling="no" frameBorder="0" allowFullScreen></iframe>
+                            </div>
                         </IonCol>
                     </IonRow>
                 </IonGrid>
+
             </IonContent>
         </IonPage>
     );
