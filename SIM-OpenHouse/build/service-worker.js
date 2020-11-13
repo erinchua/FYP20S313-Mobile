af3909ba55ad7db4366622deb55ef67a6ced8692
Vincent Yap <the-mazer@hotmail.com>
2020-11-13 23:30:37 +0800

    .

--- SIM-OpenHouse/build/service-worker.js
+++ SIM-OpenHouse/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.762dc518bad296eee7216daeb91aca08.js"
+  "/precache-manifest.5c08574c5863ab6762aecc12e91337be.js"
 );
 
 self.addEventListener('message', (event) => {
